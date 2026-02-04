7ac7280c307c8a0750c5abcc73e2a005108c8b08
Dmytro Serhiienko <conneciones@gmail.com>
2026-02-05 01:12:11 +0200

    fix: cleanup main.js imports and resolve conflicts

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './js/mobile-menu.js';
 import './js/artists.js';
-<<<<<<< HEAD
-=======
 import './js/artists-loader.js';
->>>>>>> main
 import './js/artist-details-modal.js';
-import './js/artists-loader.js';
 import './js/feedback-modal.js';
