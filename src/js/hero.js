25a451039b2f61f749bc24c90352234779c5268c
Anna Kusova <kusovanna@gmail.com>
2026-02-07 15:38:05 +0200

    edit styles hero

--- src/js/hero.js
+++
@@ -1,8 +0,0 @@
-const exploreBtn = document.querySelector("#exploreBtn");
-const artistSection = document.querySelector("#artists");
-
-exploreBtn.addEventListener('click', () => {
-    artistSection.scrollIntoView({
-        behavior: "smooth",
-    });
-});
